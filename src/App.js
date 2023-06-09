67442c4e5077fb4840c99dceb1d6b4d85a56fbdf
NadTyom <nadtyom@gmail.com>
2023-06-09 06:19:42 -0700

    Second commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main/Main";
+import Footer from "./Footer";
+// import Ruflag from "./images/ruflag.png";
+// import Armflag from "./images/armflag.png";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
